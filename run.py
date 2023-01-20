55a969354566a6543ead7fb0f4ff18a5ffad04e1
borislavtubic <101349967+borislavtubic@users.noreply.github.com>
2023-01-20 20:56:18 +0100

    Update run.py

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from projekat import app
 
 #Checks if the run.py file has executed directly and not imported
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000, host="0.0.0.0")
